09ff5cce28ee14e24d0895b8b7c03f0cd4fe58b9
py722318 <py722318@gmail.com>
2025-01-09 23:32:30 +0530

    Added Configution greeting message

--- azure-app-pankaj/Program.cs
+++ azure-app-pankaj/Program.cs
@@ -2,9 +2,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddApplicationInsightsTelemetry(new Microsoft.ApplicationInsights.AspNetCore.Extensions.ApplicationInsightsServiceOptions
+{
+    ConnectionString = builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]
+});
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
